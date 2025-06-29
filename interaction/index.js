1a4b46d6894cd41444c6f8f6594ed1d1878f588f
kanxi2 <pukakanxi@gmail.com>
2025-06-29 20:46:29 +0545

    done work

---
+++ interaction/index.js
@@ -0,0 +1,13 @@
+console.log("interaction");
+
+const say = "hello world";
+alert(say);
+alert("rashmi")
+const user=confirm("are u male?");
+console.log(user);
+
+const name = prompt("how are you ?");
+console.log(name);
+
+const class_ = prompt("In which class do you study?");
+alert(class_);
