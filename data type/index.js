50373401559ab7208b09b5af2b4d7717bd32cfe1
kanxi2 <pukakanxi@gmail.com>
2025-06-30 13:06:37 +0545

    data type

---
+++ data type/index.js
@@ -0,0 +1,48 @@
+//String;
+
+const name = "rashmi";
+console.log(name);
+
+//Number;
+const pi = 3.45566;
+console.log(pi);
+
+ //BigInt;
+const int=BigInt("7484738392398379374932840927344")
+console.log(int);
+
+//Undefine;
+let ex;
+console.log(ex);
+
+//Null;
+const X = null;
+console.log(X);
+
+//BOOLEAN
+const ismale = false;
+const isfemale = true;
+console.log(ismale,isfemale);
+
+//symbol;
+const person = "rashmi";
+console.log(person);
+ 
+//object;
+const rashmi = { name:"rashmi",age:19};
+console.log(rashmi);
+
+//type conversion;
+//string=//number;
+const num = "8";
+console.log(num);
+const actualNumber= Number(num);
+console.log(actualNumber);
+console.log(actualNumber + 2);
+
+//NUMBER=//STRING
+const num2 = 8;
+console.log(num2);
+const stringNum = String(num2);
+console.log(stringNum);
+console.log(stringNum + 2);
