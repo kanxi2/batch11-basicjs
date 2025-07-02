d43c331976fd3c9dd1bbd064630bc91f8e5bb0ac
kanxi2 <pukakanxi@gmail.com>
2025-07-02 14:12:38 +0545

    half of function

---
+++ function/index.js
@@ -0,0 +1,42 @@
+// const a = 3;
+// const b = 4;
+
+// const add  = a+b;
+
+// alert(`the sum of the two number is ${add} `)
+
+// sabman 
+// const math = 70;
+// const english = 80;
+// const sabnam_total_marks= math+english
+// alert(`the total marks of the sabnam is ${sabnam_total_marks}`)
+
+
+
+// sum marks function 
+
+//function sumMarks(math, english} //parameters
+   
+  //  const add = math+english
+    
+ //   return add }
+
+
+//const totalmarks =sumMarks(100,100) // arguments
+
+//alert(`the total marks is ${totalmarks}`)}const sumup = (a, b) => {const sum = a + b;
+  
+
+
+const sumup = (a, b) => {
+  const sum = a + b;
+  return sum;
+};
+
+const add = sumup(1, 9);
+alert(add); // Output: 10
+
+
+
+
+
